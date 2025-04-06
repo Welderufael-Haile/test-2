462c6ac56453047b70859a87a4099b0ef81f78fc
welderufaelhaile <rufaelhaile14@gmail.com>
2025-04-06 12:56:33 +0300

    update admin page

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,24 +1,9 @@
-'use client';
-import { useAuth } from '../context/AuthContext';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import React from 'react'
 
-export default function ProtectedPage() {
-  const { user } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!user) {
-      router.push('/login');
-    }
-  }, [user]);
-
-  if (!user) return null;
-
+const page = () => {
   return (
-    <div>
-      {/* your features or admin content */}
-      <h1> admin page</h1>
-    </div>
-  );
+    <div>admin page</div>
+  )
 }
+
+export default page
